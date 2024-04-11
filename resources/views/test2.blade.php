6b9924781b4f9ba03ed1b4f7365e6d39aea26ecc
SwetaImmi <sweta.k@whizkraft.net>
2024-04-11 17:31:00 +0530

    test4

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,2 +1,11 @@
-<h2>jfhjdfj</h2>
-<h1>xxxxxx</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Test</h1>
+</body>
+</html>
