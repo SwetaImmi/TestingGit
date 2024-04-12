f0ba09bf93ef537bac37c65344f06aaa73a94db7
SwetaImmi <sweta.k@whizkraft.net>
2024-04-12 10:07:17 +0530

    test2

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,2 +1,15 @@
 <h2>Hello</h2>
-dfjkhksjddfdfhkdsfhdskff
+<h2>trait_exists</h2>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+</html>
